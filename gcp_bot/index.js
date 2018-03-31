4ccd8be25f0685dd5b8517fb7dffc120f15c7edd
Gugu42 <gugo42800@gmail.com>
2018-03-31 17:11:03 +0200

    Test woow

--- gcp_bot/index.js
+++ gcp_bot/index.js
@@ -1,14 +1,48 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
+
+
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
+  client.user.setPresence({
+    game: {
+      name: "with his ass"
+    }
+  });
 });
 
+
+
+let PREFIX = '€';
+
 client.on('message', msg => {
-  if (msg.content === 'alex is a fucking') {
-    msg.reply('NIGGER');
+  const message = msg.content.trim().toLowerCase();
+
+  if (message === 'alex is a fucking') {
+    msg.channel.send('NIGGER');
+  }
+
+  if (message === 'amd') {
+    msg.channel.send('yes');
+  }
+
+  if (message === 'intel') {
+    msg.channel.send('no');
+  }
+
+  if (message === 'is sean right ?') {
+    msg.channel.send('sean is always wrong');
+  }
+
+  if (message.toLowerCase().startsWith(PREFIX.toLowerCase())) {
+    const command = message.substring(PREFIX.length).split(/[ \n]/)[0].toLowerCase().trim();
+
+    switch (command) {
+      case 'sean':
+        msg.channel.send('sean is an idiot');
+    }
   }
 });
 
-client.login('NDI5NTkyMTcyMDE5MzE4Nzk1'+ '.DaD4ug.' +'9v3CfIm-KhvCygEcTfiDWxtcWGw');
+client.login('NDI5NTkyMTcyMDE5MzE4Nzk1' + '.DaD4ug.' + '9v3CfIm-KhvCygEcTfiDWxtcWGw');
